95d831a8ad6989a6821f2ae496021e293c2cb438
pupupuwawawa <148538727+pupupuwawawa@users.noreply.github.com>
2024-01-19 19:13:48 +0900

    Add files via upload

---
+++ 813.c
@@ -0,0 +1,33 @@
+#include<float.h>
+#include<stdio.h>
+
+int main(void)
+{
+	int i;
+	double max_weight, min_weight;
+	double weight[3];
+
+	weight[0] = 72.0;
+	weight[1] = 101.5;
+	weight[2] = 52.4;
+
+	max_weight = -DBL_MAX;
+	min_weight = DBL_MAX;
+
+	for (i = 0; i < 3; i++)
+	{
+		if (weight[i] >= max_weight)
+		{
+			max_weight = weight[i];
+		}
+		if (weight[i] <= min_weight)
+		{
+			min_weight = weight[i];
+		}
+	}
+
+	printf("1”Ôd‚¢l‚Í,%g kg‚Å‚·.\n", max_weight);
+	printf("1”ÔŒy‚¢l‚Í,%g kg‚Å‚·.\n", min_weight);
+
+	return 0;
+}
